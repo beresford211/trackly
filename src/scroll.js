e726dc7a53af2d01f9478d531ffca7e0648062e5
Tim <tim.steele@foresee.com>
2017-03-21 11:04:44 -0700

    Update

---
+++ src/scroll.js
@@ -0,0 +1,55 @@
+
+mouseWatcher = function(){
+  this.listOfInputs = ['input', 'textarea', 'select'];
+  this.inputNodes = [];
+};
+
+mouseWatcher.prototype.grabAllTags = function() {
+  var inputNodesFound = [],listOfTagsLeng = this.listOfInputs.length;
+
+  for (var i = 0; i < listOfTagsLeng; i++) {
+    this.inputNodes.concat(document.getElementsByTagName(this.listOfInputs[i]));
+  }
+};
+
+mouseWatcher.prototype.addListeners = function() {
+  var inputNodeLeng = this.inputNodes.length;
+
+  for (var i = 0; i < inputNodeLeng; i++) {
+    // input
+    // utilityfunctions.addListener
+  }
+};
+
+mouseWatcher.prototype.updateScroll = function(el){
+
+
+};
+
+mouseWatcher.prototype.xPath = function(el){
+  var xpath = '';
+  var pos, el2;
+  var posText;
+
+  while(el) {
+    pos = 0;
+    el2 = el;
+    while(el2) {
+      if (el2.nodeType === 1 && el2.nodeName === el.nodeName) { // If it is ELEMENT_NODE of the same name
+        pos += 1;
+      }
+      el2 = tempitem2.previousSibling;
+    }
+    posText = "";
+    if(pos > 0) {
+      posText = "[" + pos + "]";
+    }
+
+    tempXpath = "" + el.nodeName + "" + ( el.id === null ? (pos > 0 ? posText : "" ) + "" : "[@id='" + el.id + "']" + "[" + pos + "]");
+
+    el = el.parentNode;
+  }
+  xpath = '/*'+"[name()='"+xml.documentElement.nodeName+"' and namespace-uri()='"+(el.namespaceURI===null?'':el.namespaceURI)+"']"+'/'+xpath;
+  xpath = xpath.replace(/\/$/, '');
+  return xpath;
+};
