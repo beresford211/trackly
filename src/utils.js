875563b609410cb7eb6fd98d199c6fd86dae8623
Tim Steele <beresford211@users.noreply.github.com>
2017-03-24 14:58:45 -0700

    Added utility functions as well as function to handle pointer events (#5)
    * Added the ability to test for local storage and store data in it
    
    * Added the ability to test for local storage and store data in it
    
    * Created utility functions to be used throughout the codebase
    
    * Added functions to listen for pointer events

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 module.exports = (function(){
-  
+
   var polyBind = function(func, context){
     var arr = Array.prototype.slice.call(arguments, 2);
     return function(){
@@ -9,9 +9,8 @@ module.exports = (function(){
     };
   };
 
-  var addListener = function(eventName, node, result){
+  var addListener = function(node, eventName, result){
     node.addEventListener(eventName, result);
-    return node;
   };
 
   var addListenerById = function(eventName, id, result){
@@ -32,11 +31,64 @@ module.exports = (function(){
     node.addEventListener(eventName, result);
   };
 
+  var xPath = function(el){
+    var xpath = '';
+    var pos, el2;
+    var posText;
+    while(el) {
+      console.log("waht is the el id", el.id);
+      pos = 0;
+      el2 = el;
+      while(el2) {
+        if (el2.nodeType === 1 && el2.nodeName === el.nodeName) { // If it is ELEMENT_NODE of the same name
+          pos += 1;
+        }
+        el2 = el2.previousSibling;
+      }
+      posText = pos > 0 ? "[" + pos + "]" : "";
+      console.log("waht is el", el, el.id);
+      if(el){
+        xpath = "" + el.nodeName + "" + ( el.id !== true ? posText + "" : "[@id='" + el.id + "']" + posText) + "/" + xpath;
+      }
+      el = el.parentNode;
+    }
+    // xpath = '/*'+"[name()='"+xml.documentElement.nodeName+"' and namespace-uri()='"+(el.namespaceURI===null?'':el.namespaceURI)+"']"+'/'+xpath;
+    // xpath = xpath.replace(/\/$/, '');
+    console.log("xpath", xpath);
+    return xpath;
+  };
+
+//   var throttle = function(fn, threshhold, scope) {
+//   threshhold || (threshhold = 250);
+//   var last,
+//       timerId;
+//   return function () {
+//     var context = scope || this,
+//         now = +new Date,
+//         args = arguments;
+//
+//     if (last && now < last + threshhold) {
+//       // hold on to it
+//       clearTimeout(timerId);
+//       timerId = setTimeout(function () {
+//         last = now;
+//         fn.apply(context, args);
+//       }, threshhold);
+//     } else {
+//       last = now;
+//       fn.apply(context, args);
+//     }
+//   };
+// }
+
+
+
   return {
     polyBind : polyBind,
     addListener : addListener,
     addListenerById : addListenerById,
-    addListenerByTag : addListenerByTag
+    addListenerByTag : addListenerByTag,
+    xPath : xPath
   };
 
 })();
