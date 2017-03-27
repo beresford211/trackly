5bf9fac9bdf6c99044ce9b7831db05d111c5c8c4
Tim <tim.steele@foresee.com>
2017-03-27 14:35:21 -0700

    Updated styling of code adding proper function spacing for readability

--- src/pointerWatcher.js
+++ src/pointerWatcher.js
@@ -1,22 +1,22 @@
 var utils = require("./utils.js");
 var storage = require("./storage.js");
 
-var PointerWatcher = function(){
+var PointerWatcher = function () {
   // this.nodesCollected = [];
 };
 
-PointerWatcher.prototype.bindPointer = function() {
+PointerWatcher.prototype.bindPointer = function () {
   utils.addListener(window.document.body, "pointermove", utils.throttle(this.captureMovements, 35, this));
 };
 
-PointerWatcher.prototype.captureMovements = function(e) {
+PointerWatcher.prototype.captureMovements = function (e) {
   var eventData = {};
-    if(e.type === "pointermove") {
-      eventData.clientX = e.clientX;
-      eventData.clientY = e.clientY;
-      eventData.timeStamp = e.timeStamp;
-      storage.addToLocalStorage(eventData);
-    }
+  if (e.type === "pointermove") {
+    eventData.clientX = e.clientX;
+    eventData.clientY = e.clientY;
+    eventData.timeStamp = e.timeStamp;
+    storage.addToLocalStorage(eventData);
+  }
 };
 
 module.exports = PointerWatcher;
