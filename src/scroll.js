d14ed12e6bab6c7335367111b8384c487f12702b
Tim <tim.steele@foresee.com>
2017-03-26 21:17:51 -0700

    Refactored scroll to be simpiler

--- src/scroll.js
+++ src/scroll.js
@@ -1,76 +1,43 @@
 var utils = require("./utils.js");
+var storage = require("./storage.js");
 
 var DocumentWatcher = function(){
-  this.listOfInputs = ['input', 'textarea', 'select'];
   this.inputNodes = [];
 };
 
-DocumentWatcher.prototype.grabAllTags = function() {
-  var inputNodesFound = [],listOfTagsLeng = this.listOfInputs.length;
-
-  for (var i = 0; i < listOfTagsLeng; i++) {
-    this.inputNodes.concat(document.getElementsByTagName(this.listOfInputs[i]));
-  }
+DocumentWatcher.prototype.bindMainEvents = function() {
+  utils.addListener(window, "resize", this.windowSize);
+  utils.addListener(window, "scroll", this.updateScroll, true);
+  utils.addListener(window, "click", this.clickCapture);
+  utils.addListener(window, "beforeunload", utils.removeListeners);
 };
 
-DocumentWatcher.prototype.bindMainEvents = function(){
-    utils.addListener(window, "resize", this.resize);
+DocumentWatcher.prototype.updateScroll = function(e) {
+  var eventData = {};
+
+  eventData.scrollTop = window.pageYOffset;
+  eventData.scrollLeft = window.pageXOffset;
+  eventData.timeStamp = e.timeStamp;
+  storage.addToLocalStorage(eventData);
 };
 
+DocumentWatcher.prototype.windowSize = function() {
+  var eventData = {};
 
-
-DocumentWatcher.prototype.addListeners = function() {
-  var inputNodeLeng = this.inputNodes.length;
-
-  for (var i = 0; i < inputNodeLeng; i++) {
-    // utilityfunctions.addListener
-  }
+  eventData.windowScreen = document.documentElement.clientWidth;
+  eventData.windowHeight = document.documentElement.clientHeight;
+  eventData.windowSizeTime = Math.floor(Date.now() / 1000);
+  storage.addToLocalStorage(eventData);
 };
 
-DocumentWatcher.prototype.updateScroll = function(el){
+DocumentWatcher.prototype.clickCapture = function(e){
+  var eventData = {}, doc = e.srcElement.getBoundingClientRect();
 
-};
-
-DocumentWatcher.prototype.documentSize = function(){
-  var windowScreen = window.screen.width,
-  windowHeight = window.screen.height;
-};
-
-DocumentWatcher.prototype.resize = function(e){
-
-
-};
-
-
-
-
-DocumentWatcher.prototype.xPath = function(el){
-  var xpath = '';
-  var pos, el2;
-  var posText;
-
-  while(el) {
-    pos = 0;
-    el2 = el;
-    while(el2) {
-      if (el2.nodeType === 1 && el2.nodeName === el.nodeName) { // If it is ELEMENT_NODE of the same name
-        pos += 1;
-      }
-      el2 = tempitem2.previousSibling;
-    }
-    posText = "";
-    if(pos > 0) {
-      posText = "[" + pos + "]";
-    }
-
-    xpath = "" + el.nodeName + "" + ( el.id === null ? (pos > 0 ? posText : "" ) + "" : "[@id='" + el.id + "']" + "[" + pos + "]") + "/" + xpath;
-
-    el = el.parentNode;
-  }
-console.log("what is xpath", xpath);
-  // xpath = '/*'+"[name()='"+xml.documentElement.nodeName+"' and namespace-uri()='"+(el.namespaceURI===null?'':el.namespaceURI)+"']"+'/'+xpath;
-  // xpath = xpath.replace(/\/$/, '');
-  return xpath;
+  eventData.timeStamp = e.timeStamp;
+  eventData.xCoordinate = doc.left + e.pageXOffset;
+  eventData.xpath = this.xPath(e.target.activeElement);
+  eventData.yCoordinate = doc.top + e.pageYOffset;
+  storage.addToLocalStorage(eventData);
 };
 
 module.exports = DocumentWatcher;
